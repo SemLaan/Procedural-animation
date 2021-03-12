dd19bb1e8f2f57488d5f481fcb5d9956d713b0c1
sem laan <semlaan@dds.nl>
2021-03-12 16:34:25 +0100

    The creature's body now gets set to the correct rotation and position when the scene starts

--- Assets/Scripts/ProceduralAnimation/TwoLeggedSpiderController.cs
+++ Assets/Scripts/ProceduralAnimation/TwoLeggedSpiderController.cs
@@ -7,13 +7,22 @@ public class TwoLeggedSpiderController : MonoBehaviour
     [Header("Objects")]
     [SerializeField] private Transform leftLegTarget;
     [SerializeField] private Transform rightLegTarget;
+    [SerializeField] private Transform leftLeg;
+    [SerializeField] private Transform rightLeg;
     [Header("Variables")]
+    [SerializeField] private float bodyToGroundDistance;
     [SerializeField] private float legWidth;
     [SerializeField] private LayerMask groundLayer;
 
     private void Awake()
     {
         InitLegTargetPositions();
+        AlignBody();
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void InitLegTargetPositions()
@@ -27,6 +36,16 @@ public class TwoLeggedSpiderController : MonoBehaviour
         rightLegTarget.position = transform.position + new Vector3(legWidth, -rightLegRay.distance);
         leftLegTarget.rotation = Quaternion.Euler(0, 0, -90);
         rightLegTarget.rotation = Quaternion.Euler(0, 0, -90);
+    }
+
+    private void AlignBody()
+    {
+        Ray rayDownFromBody = new Ray(transform.position, -transform.up);
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(rayDownFromBody, out hit, 100, groundLayer.value);
+        transform.position = hit.point + hit.normal * bodyToGroundDistance;
+
+        transform.rotation *= Quaternion.FromToRotation(transform.up, hit.normal);
     }
 
     private void OnDrawGizmosSelected()
